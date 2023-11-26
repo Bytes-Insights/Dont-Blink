8186c64fbd9050f666a422678d93ae997feed4ef
Markus Wagner <prttmprpht@gmail.com>
2023-11-26 16:43:11 +0100

    Improve angel movement

--- Assets/Scripts/Interaction/Response/SimpleAngelMovement.cs
+++ Assets/Scripts/Interaction/Response/SimpleAngelMovement.cs
@@ -7,22 +7,33 @@ public class SimpleAngelMovement : MonoBehaviour
     public InteractionMediator mediator;
     public GameObject trackedObject;
     public float speed;
+    public float minOpenTimeForReset = 0.1F;
 
-    void Start()
+    private bool lastCapturedEyeState = false;
+    private float openTime = 0F;
+
+    void MoveAngel()
     {
-        mediator.OnEyesStatusChanged += OnEyesChanged;
+        Vector3 currPos = new Vector3(transform.position.x, 0, transform.position.z);
+        Vector3 trackedPos = new Vector3(trackedObject.transform.position.x, 0, trackedObject.transform.z);
+        Vector3 result = Vector3.MoveTowards(currPos, trackedPos, speed);
+        transform.position = new Vector3(result.x, transform.position.y, result.z);
     }
 
-    void OnDestroy()
+    void Update()
     {
-        mediator.OnEyesStatusChanged -= OnEyesChanged;
-    }
-
-    void OnEyesChanged(bool closed)
-    {
-        if (closed)
+        if (mediator.EyesClosed() && !lastCapturedEyeState)
         {
-            transform.position = Vector3.MoveTowards(transform.position, trackedObject.transform.position, speed);
+            lastCapturedEyeState = true;
+            MoveAngel();
+        } else if (!mediator.EyesClosed() && lastCapturedEyeState)
+        {
+            openTime += Time.deltaTime;
+            if (openTime > minOpenTimeForReset)
+            {
+                lastCapturedEyeState = false;
+                openTime = 0F;
+            }
         }
     }
 }
