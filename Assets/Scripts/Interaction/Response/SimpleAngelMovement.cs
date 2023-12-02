4289c5c51c35b80b8f434e6a8eabdf6834f3111e
Ferran Campos <ferran98campos@gmail.com>
2023-12-02 16:21:38 +0100

    Basic Room Crawling

--- Assets/Scripts/Interaction/Response/SimpleAngelMovement.cs
+++ Assets/Scripts/Interaction/Response/SimpleAngelMovement.cs
@@ -10,29 +10,88 @@ public class SimpleAngelMovement : MonoBehaviour
     public float speed;
     public float minOpenTimeForReset = 0.1F;
     public NavMeshAgent agent;
+    public bool moveOnNotSeen;
+    public float angelViewDistance;
+    public float wanderRadius;
 
     private bool lastCapturedEyeState = false;
     private float openTime = 0F;
+    private bool chasingMode = false;
+    private Vector3 targetPosition;
+    private float distanceThreshold = 3.0f;
+    private GameObject[] rooms;
 
-    void MoveAngel()
+    void Start()
     {
-        /*
-        Vector3 currPos = new Vector3(transform.position.x, 0, transform.position.z);
-        Vector3 trackedPos = new Vector3(trackedObject.transform.position.x, 0, trackedObject.transform.position.z);
-        Vector3 result = Vector3.MoveTowards(currPos, trackedPos, speed);
-        transform.position = new Vector3(result.x, transform.position.y, result.z);
-        */
-
+        //Original target position = self
+        targetPosition = transform.position;
+        rooms = GameObject.FindGameObjectsWithTag("Room");
     }
 
     void Update()
     {
-        //agent.SetDestination(trackedObject.transform.position);
+        MoveAngel();
+    }
+
+    //Returns true if angel can clearly see the player in the given distance and without obstacles in between
+    bool seePlayer()
+    {
+        RaycastHit objectHit;
+        Ray ray = new Ray(transform.position, trackedObject.transform.position - transform.position);
+        //casting a ray against the player
+        if (Physics.Raycast(ray, out objectHit, angelViewDistance))
+        {
+            if(objectHit.collider.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("DETECTED!!!!");
+                return true;
+            }
+        }
+        Debug.Log("NOTHING");
+        return false;
+    }
+
+    //Returns next room position to be checked by angel
+    Vector3 getNextTargetRoom()
+    {
+        return rooms[Random.Range(0, rooms.Length)].transform.position;
+    }
+
+    void Wander()
+    {   
+        //If the angel has reached the room, get a new random room and go check it
+        if(Vector3.Distance(targetPosition, transform.position) < distanceThreshold)
+        {
+            //Selects a random target point in the maze and updates it if the angel reached it
+            targetPosition = getNextTargetRoom();
+            agent.SetDestination(targetPosition);
+        }
+
+        // TODO: This can be more complex. Angel should be able to detect sounds. Maybe the angel should wander to a random position close to the player
+    }
+
+    void MoveAngel()
+    {
+        //Angel enters chasing mode as soon as player is seen
+        if(seePlayer())
+            chasingMode = true;
+
+        //Wanders in the maze or travels towards the player depending on chasingMode. 
+        if(chasingMode)
+            agent.SetDestination(trackedObject.transform.position);
+        else 
+            Wander();
+        
+    }
+
+    void MoveOnBlink()
+    {
         if (mediator.EyesClosed() && !lastCapturedEyeState)
         {
             lastCapturedEyeState = true;
             agent.SetDestination(trackedObject.transform.position);
             agent.isStopped = false;
+            
         } else if (!mediator.EyesClosed() && lastCapturedEyeState)
         {
             openTime += Time.deltaTime;
