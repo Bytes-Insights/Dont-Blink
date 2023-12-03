9b792750e4523be67fabeec0379ff04562ee5fe5
Markus Wagner <prttmprpht@gmail.com>
2023-12-03 16:11:35 +0100

    Add NoAI movement script for testing in scenes without NavMesh..

---
+++ Assets/Scripts/Interaction/Response/NoAIAngelMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoAIAngelMovement : MonoBehaviour
+{
+    public InteractionMediator mediator;
+    public GameObject trackedObject;
+    public float speed;
+    public float minOpenTimeForReset = 0.1F;
+
+    private bool lastCapturedEyeState = false;
+    private float openTime = 0F;
+
+    void MoveAngel()
+    {
+        Vector3 currPos = new Vector3(transform.position.x, 0, transform.position.z);
+        Vector3 trackedPos = new Vector3(trackedObject.transform.position.x, 0, trackedObject.transform.position.z);
+        Vector3 result = Vector3.MoveTowards(currPos, trackedPos, speed);
+        transform.position = new Vector3(result.x, transform.position.y, result.z);
+    }
+
+    void Update()
+    {
+        if (mediator.EyesClosed() && !lastCapturedEyeState)
+        {
+            lastCapturedEyeState = true;
+            MoveAngel();
+        }
+        else if (!mediator.EyesClosed() && lastCapturedEyeState)
+        {
+            openTime += Time.deltaTime;
+            if (openTime > minOpenTimeForReset)
+            {
+                lastCapturedEyeState = false;
+                openTime = 0F;
+            }
+        }
+    }
+}
