4151ef21a17f84997a20780d2539ea516db7ff7b
Markus Wagner <prttmprpht@gmail.com>
2023-12-10 00:05:44 +0100

    Add comment to new player movement script

--- Assets/Scripts/Player/Input/PlayerControls.cs
+++ Assets/Scripts/Player/Input/PlayerControls.cs
@@ -37,6 +37,9 @@ public class PlayerControls : MonoBehaviour
         float forward = inputActions.Movement.Forward.ReadValue<float>();
         float right = inputActions.Movement.Right.ReadValue<float>();
 
+        // TODO: Instead of using rigidbody.velocity we might want to use the movement logic from the old script
+        //       But then we should make sure that the rigidbody itself has the X and Z position locked.
+        //       We should keep it for the Y direction, as that will ensure our character is properly grounded.
         Vector3 movement = (transform.right * right + transform.forward * forward) * speed;
         rigidbody.velocity = new Vector3(movement.x, rigidbody.velocity.y, movement.z);
     }
