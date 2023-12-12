aa577514da44ba733ee360f88c6bc6c851f494f2
Markus Wagner <prttmprpht@gmail.com>
2023-12-13 00:30:37 +0100

    Add script for dial

---
+++ Assets/Scripts/Puzzles/RadioPuzzle/Dial.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dial : MonoBehaviour
+{
+    public GameObject renderPlane;
+    public InteractionMediator mediator;
+    public float speed = 10F;
+
+    private Material renderPlaneMaterial;
+    private float rotation = 0F;
+
+    public void Start()
+    {
+        renderPlaneMaterial = renderPlane.GetComponent<MeshRenderer>().material;
+    }
+
+    public void Update()
+    {
+        float newRotation = rotation + speed * (mediator.FaceDirectionalWeight() * -1) * Time.deltaTime;
+        rotation = Mathf.Clamp(newRotation, -90, 90);
+
+        renderPlaneMaterial.SetFloat("_Rotation", rotation);
+    }
+}
