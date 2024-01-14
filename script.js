118b35934117e4f7f550cccfef9c95c20dcea807
tiagombp <tiagombp@gmail.com>
2024-01-14 17:49:20 +0100

    that commit that you do when you realize you forgot committing 37426 times

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ for (let i = 0; i < nPaths; i++) {
     const x2 = hAttrSVG / 2 + ( r * Math.sin(theta * i) ) * factor;
     const x1 = hAttrSVG / 2 + ( r0 * Math.sin(theta * i) ) * factor;
 
-    const y2 = hAttrSVG / 2 + (r * Math.cos(theta * i)  )* factor;
-    const y1 = hAttrSVG / 2 + (r0 * Math.cos(theta * i) ) * factor;
+    const y2 = ( hAttrSVG / 2 + (r * Math.cos(theta * i)  )* factor );
+    const y1 = ( hAttrSVG / 2 + (r0 * Math.cos(theta * i) ) * factor );
 
     points.push({x1,y1,x2,y2});
 
@@ -53,6 +53,49 @@ paths.forEach( (path,i) => {
 
     const {x1, y1, x2, y2} = points[i];
 
+    const xm = ( x1 + x2 ) / 2
+    const ym = ( y1 + y2 ) / 2
+    /*
+    const xm1 = ( x1 + x2 ) / 3 + 3;
+    const ym1 = ( y1 + y2 ) / 3;
 
-    path.setAttribute("d", `M${x1} ${y1}L${x2} ${y2}`);
+    const xm2 = 2 * ( x1 + x2 ) / 3 - 3;
+    const ym2 = 2 * ( y1 + y2 ) / 3;
+    */
+
+    const desiredPath = `M ${x1} ${y1} L ${xm} ${ym} L ${x2} ${y2}`;
+
+    path.setAttribute("d", desiredPath);
+
+    console.log(desiredPath);
+
+    //path.currentPath = desiredPath;
+
 })
+
+const pd3 = d3.selectAll(".dont-blink path");
+
+function bouge() {
+    pd3.transition().duration(1500)
+    .style("opacity", 1)
+
+    pd3.transition().delay(1000 + ( Math.random() - .5) * 400).duration((d,i) => 3000 + (Math.random() - 0.5) * 500)
+    .attrTween("d", function(d,i) {
+
+        const d0 = d3.select(this).attr("d");
+
+        return flubber.interpolate(d0, pathsData[i]);
+
+    })
+
+    d3.select('.eye').transition().duration(3000).style("opacity", 0);
+}
+
+bouge();
+/*
+pd3.attr("d", (d,i) => {
+
+    const {x1, y1, x2, y2} = points[i];
+    return `M${x1} ${y1}L${x2} ${y2}`;
+
+})*/
